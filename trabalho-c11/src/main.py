d18323fb04e03d9518036b107feaf718579b1b17
JVC2511 <jvacd2003@gmail.com>
2025-10-20 09:26:32 -0300

    adicionando o resto das perguntas

--- trabalho-c11/src/main.py
+++ trabalho-c11/src/main.py
@@ -63,3 +63,31 @@ print("\n=== Quais empresas mais contratam profissionais de Data Science? ===")
 empresas_mais_ativas = df['company'].value_counts().head(10)
 print(empresas_mais_ativas.to_string())
 #TODO: MELHORAR ESSA PERGUNTA 
+
+#6 Qual o nivel de experiencia mais requisitado pelas empresas?
+print("\n=== Qual o nivel de experiencia mais requisitado pelas empresas? ===")
+nivel_exp = df['seniority_level'].value_counts()
+print("\nNiveis de experiencia mais requisitados pelas empresas:")
+print(nivel_exp)
+
+#7 Quais sao as 5 empresas com mais receita no mercado?
+print("\n=== Quais as 5 empresas com mais receita no mercado? ===")
+
+#8 Quais sao as habilidades mais requisitadas pelas empresas?
+
+#print("\n=== Quais sao as habilidades mais requisitadas pelas empresas? ===")
+#df['skills'] = df['skills'].apply(lambda x: ast.literal_eval(x) if pd.notnull(x) else [])
+#todas_habilidades = [hab for sublista in df['habilidades'] for hab in sublista]
+#contagem = Counter(todas_habilidades)
+#top3 = contagem.most_common(3)
+#print("As 3 habilidades mais comuns são:")
+#for skills, freq in top3:#print(f"{skills}: {freq}"}
+
+#9 Qual tipo de empresa (Publica ou Privada) que melhor paga os funcionarios?
+df["salario_medio"] = df["salary"].mean()
+df = df.dropna(subset=["salario_medio", "ownership"])
+media_por_tipo = df.groupby("ownership")["salario_medio"].mean().sort_values(ascending=False)
+print("Salário médio por tipo de empresa (€):")
+print(media_por_tipo)
+
+#10 Qual setor da Industria mais paga os funcionarios?
