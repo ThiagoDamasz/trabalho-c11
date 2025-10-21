9eb929b3574189532d2ceef2413bfb26b7c2189c
Henrique Issao Kato <114307623+HenriqueIK@users.noreply.github.com>
2025-10-20 23:22:51 -0300

    Arrumei as perguntas so falta 2

--- trabalho-c11/src/main.py
+++ trabalho-c11/src/main.py
@@ -8,10 +8,8 @@ delimiter = ',')
 
 print(df.columns)
 
-
 # Formatação geral do DataFrame
 pd.options.display.float_format = '{:.2f}'.format
-
 
 # Formatação da coluna salary
 
@@ -29,23 +27,18 @@ df['salary_max'] = df['salary_max'].str.replace(',', '').astype(float)
 # Criar coluna de salário médio
 df['salary_avg'] = df[['salary_min', 'salary_max']].mean(axis=1)
 
-
 # Formatação da coluna location
 df['country'] = df['location'].str.split(',').str[2].str.strip()
 
-
-
-#Perguntas:
-
 #1 Qual o salário médio por cargo?
-salario_medio_por_cargo = df.groupby('job_title')['salary_avg'].mean().to_string()
-print("\n=== Qual o salário médio por cargo? ===")
-print(salario_medio_por_cargo)
+print("\n=== Qual o salário médio por cargo(anual)? ===")
+salario_cargo = df.groupby('job_title')['salary_avg'].mean().to_string()
+print(salario_cargo)
 
 #2 Qual a diferença de salário médio por nível de senioridade (junior, pleno, senior) ?
-salario_medio_por_senioridade = df.groupby('seniority_level')['salary_avg'].mean().to_string()
 print("\n=== Qual o salário médio por nível de senioridade? ===")
-print(salario_medio_por_senioridade)
+salario_senioridade = df.groupby('seniority_level')['salary_avg'].mean().to_string()
+print(salario_senioridade)
 
 #3 Qual o salário médio por regime de trabalho (hibrído, presencial ou remoto) ?
 salario_medio_por_regime = df.groupby('status')['salary_avg'].mean().to_string()
@@ -53,41 +46,48 @@ print("\n===Qual o salário médio por regime de trabalho? ===")
 print(salario_medio_por_regime)
 
 #4 Qual país paga os maiores salários para profissionais de Data Science? 
-salario_medio_por_localizacao = df.groupby('country')['salary_avg'].mean().to_string()
 print("\n===Quais são os países que possuem os melhores salários? ===")
-print(salario_medio_por_localizacao)
-#TODO: CONSERTAR A FORMATAÇÃO DESSE DATASET
+salario_localizacao = df.groupby('country')['salary_avg'].mean().to_string()
+print(salario_localizacao)
+#Melhorar a resposta que so esta mostrando em ordem alfabetica
 
 #5 Qual empresa contrata mais profissionais de Data Science?
 print("\n=== Quais empresas mais contratam profissionais de Data Science? ===")
-empresas_mais_ativas = df['company'].value_counts().head(10)
-print(empresas_mais_ativas.to_string())
-#TODO: MELHORAR ESSA PERGUNTA 
+empresas_cont = df['company'].value_counts().head(10)
+print(empresas_cont.to_string())
 
 #6 Qual o nivel de experiencia mais requisitado pelas empresas?
 print("\n=== Qual o nivel de experiencia mais requisitado pelas empresas? ===")
-nivel_exp = df['seniority_level'].value_counts()
-print("\nNiveis de experiencia mais requisitados pelas empresas:")
+nivel_exp = df['seniority_level'].value_counts().to_string()
 print(nivel_exp)
 
 #7 Quais sao as 5 empresas com mais receita no mercado?
 print("\n=== Quais as 5 empresas com mais receita no mercado? ===")
 
 #8 Quais sao as habilidades mais requisitadas pelas empresas?
+print("\n=== Quais sao as habilidades mais requisitadas pelas empresas? ===")
+df['skills_clean'] = (
+    df['skills']
+    .astype(str)
+    .str.replace('[\[\]\']', '', regex=True)
+)
+df['skills_list'] = df['skills_clean'].str.split(', ')
 
-#print("\n=== Quais sao as habilidades mais requisitadas pelas empresas? ===")
-#df['skills'] = df['skills'].apply(lambda x: ast.literal_eval(x) if pd.notnull(x) else [])
-#todas_habilidades = [hab for sublista in df['habilidades'] for hab in sublista]
-#contagem = Counter(todas_habilidades)
-#top3 = contagem.most_common(3)
-#print("As 3 habilidades mais comuns são:")
-#for skills, freq in top3:#print(f"{skills}: {freq}"}
+df_exploded = df.explode('skills_list')
+df_exploded['skills_list'] = df_exploded['skills_list'].str.strip()
+df_exploded = df_exploded[df_exploded['skills_list'].astype(bool)]
+
+habilidades = df_exploded['skills_list'].value_counts().head(10)
+print(habilidades)
 
 #9 Qual tipo de empresa (Publica ou Privada) que melhor paga os funcionarios?
-df["salario_medio"] = df["salary"].mean()
-df = df.dropna(subset=["salario_medio", "ownership"])
-media_por_tipo = df.groupby("ownership")["salario_medio"].mean().sort_values(ascending=False)
-print("Salário médio por tipo de empresa (€):")
-print(media_por_tipo)
+print("\n=== Qual o tipo de empresa (Public ou Private) está pagando mais? ===")
+df = df.dropna(subset=["ownership", "salary_avg"])
+tipo_emp = df.groupby("ownership")["salary_avg"].mean().sort_values(ascending=False)
+print(tipo_emp)
 
-#10 Qual setor da Industria mais paga os funcionarios?
+#10 Quais setores da Industria que menos utilizam data science?
+print("\n=== Qual setor da Industria contrata menos profissionais de data science? ===")
+industria = df['industry'].value_counts().sort_values()
+print("\nIndustrias que menos utilizam:")
+print(industria.head(3))
