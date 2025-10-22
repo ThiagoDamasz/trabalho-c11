dd9e9b8fea1835658dc5e066ce5b13d3a1bdd1e3
Henrique Issao Kato <114307623+HenriqueIK@users.noreply.github.com>
2025-10-21 22:06:02 -0300

    Terminando o codigo e graficos

--- trabalho-c11/src/main.py
+++ trabalho-c11/src/main.py
@@ -9,26 +9,18 @@ delimiter = ',')
 
 print(df.columns)
 
-# Formatação geral do DataFrame
 pd.options.display.float_format = '{:.2f}'.format
 
-# Formatação da coluna salary
-
-# tirando os caracteres e espacos da coluna salary
 df['salary_clean'] = df['salary'].str.replace('€', '').str.replace(' ', '')
 
-# criando duas colunas para o salário mínimo e máximo
 df['salary_min'] = df['salary_clean'].str.split('-').str[0]
 df['salary_max'] = df['salary_clean'].str.split('-').str[1]
 
-# Remover vírgulas e converter para float
 df['salary_min'] = df['salary_min'].str.replace(',', '').astype(float)
 df['salary_max'] = df['salary_max'].str.replace(',', '').astype(float)
 
-# Criar coluna de salário médio
 df['salary_avg'] = df[['salary_min', 'salary_max']].mean(axis=1)
 
-# Formatação da coluna location
 df['country'] = df['location'].str.split(',').str[2].str.strip()
 
 #1 Qual o salário médio por cargo?
@@ -59,17 +51,22 @@ salario_medio_por_regime = df.groupby('status')['salary_avg'].mean().to_string()
 print("\n===Qual o salário médio por regime de trabalho? ===")
 print(salario_medio_por_regime)
 
-#4 Qual localização paga os maiores salários para profissionais de Data Science? 
-print("\n=== Quais são as 5 localizações com os melhores salários? ===")
+#4 Quais localizacoes mais pagam salários para profissionais de Data Science? 
+print("\n===Quais são as 5 localizações das empresas que pagam os maiores salários anuais? ===")
+salario_localizacao = df.groupby('country')['salary_avg'].mean().sort_values(ascending=False)
+localizacao = salario_localizacao.head(5)
+print(localizacao)
 
-salario_por_pais = df.groupby('country')['salary_avg'].mean()
+plt.figure(figsize=(10,6))
+plt.barh(localizacao.index[::-1], localizacao.values[::-1], color='violet', height=0.6)
+plt.title('5 lugares com maiores pagamentos', fontsize=14)
+plt.ylabel('Localização', fontsize=12)
+plt.xlabel('Salário médio anual (€)', fontsize=11)
+plt.grid(axis='x', alpha=0.6)
+plt.tight_layout()
+plt.show()
 
-# Ordenar do maior para o menor e pegar os 5 primeiros
-top5_paises = salario_por_pais.sort_values(ascending=False).head(5)
-
-print(top5_paises)
-
-#5 Qual empresa contrata mais profissionais de Data Science?
+#5 Quais empresas contrata mais profissionais de Data Science?
 print("\n=== Quais empresas mais contratam profissionais de Data Science? ===")
 empresas_cont = df['company'].value_counts().head(5)
 print(empresas_cont.to_string())
@@ -84,8 +81,22 @@ plt.show()
 
 #6 Qual o nivel de experiencia mais requisitado pelas empresas?
 print("\n=== Qual o nivel de experiencia mais requisitado pelas empresas? ===")
-nivel_exp = df['seniority_level'].value_counts().to_string()
+nivel_exp = df['seniority_level'].value_counts()
 print(nivel_exp)
+
+plt.figure(figsize=(8, 8))
+plt.pie(
+    nivel_exp.values,
+    labels=nivel_exp.index,
+    autopct='%1.1f%%',
+    startangle=140,
+    colors=plt.cm.tab10.colors,
+    wedgeprops={'edgecolor': 'white'}
+)
+
+plt.title('Os níveis de experiencia mais requisitados pelas empresas', fontsize=14)
+plt.tight_layout()
+plt.show()
 
 #7 Quais sao as 5 empresas com mais receita no mercado? (numpy)
 print("\n=== Quais as 5 empresas com mais receita no mercado? ===")
@@ -95,7 +106,6 @@ size = df['company_size'].to_numpy()
 size_str = np.array(size, dtype=str)
 size_str[size_str == 'nan'] = '0'
 
-# Limpeza dos caracteres
 size_clean = np.char.replace(size_str, '"', '')
 size_clean = np.char.replace(size_clean, '€', '')
 size_clean = np.char.replace(size_clean, ',', '')
@@ -103,26 +113,18 @@ size_clean = np.char.replace(size_clean, 'B', '')
 size_clean = np.char.replace(size_clean, '.', '')
 size_clean = np.char.replace(size_clean, 'Private', '')
 
-# Colocar 0 onde não for número
 size_clean = np.where(np.char.isdigit(size_clean), size_clean, '0')
 
-# Converter para inteiro
 size_num = size_clean.astype(int)
 
-# Remover repetições de empresas (mantendo o primeiro registro)
 unique_companies, unique_indices = np.unique(company, return_index=True)
 unique_sizes = size_num[unique_indices]
 
-# Pegar os índices das 5 maiores empresas únicas
 top_idx = np.argsort(unique_sizes)[-5:][::-1]
 
-# Exibir resultado com for, linha por linha
 print("As 5 empresas com mais receita no mercado são:")
 for i in top_idx:
     print(f"{unique_companies[i]} ({unique_sizes[i]})")
-
-
-#alguem arruma essa pergunta ai porfavor pois nao consegui
 
 #8 Quais sao as habilidades mais requisitadas pelas empresas?
 print("\n=== Quais sao as habilidades mais requisitadas pelas empresas? ===")
